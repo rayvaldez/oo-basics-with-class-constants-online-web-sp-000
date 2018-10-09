ebc12737ce3c5990f8b489a9c87caeed3e30527c
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-10-09 17:34:21 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    BRANDS.include? (brand)
     BRANDS << brand
   end
 
@@ -13,5 +14,5 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-  
+
 end
