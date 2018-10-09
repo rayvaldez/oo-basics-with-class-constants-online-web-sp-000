8b49c301381e23f109d66490f957e27801f9bac1
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-10-09 17:38:39 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,10 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.include?(brand)
-      
-    else
-      BRANDS << brand
+    BRANDS.include?(brand) BRANDS << brand
     end
   end
 
